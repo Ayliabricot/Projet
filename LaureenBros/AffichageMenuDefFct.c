cb514f7b97bcf893bf2d2debbe08a5866b0335b4
Hugo.V <hugo.vialaneix@gmail.com>
2025-02-05 10:29:07 +0100

    nouveau

--- LaureenBros/AffichageMenuDefFct.c
+++ LaureenBros/AffichageMenuDefFct.c
@@ -1,17 +1,24 @@
 #include <windows.h>
 #include <stdio.h>
-
-
-int main() {
+#include "affichageMenu.h"
+//hugo
+//penser à faire une fonction pour free les malloc
+int tailleEcran() {
     CONSOLE_SCREEN_BUFFER_INFO csbi;
-    int width, height;
+    int* largeur;
+    int* hauteur;
+    largeur = malloc(sizeof(int));
+    hauteur = malloc(sizeof(int));
+    if (largeur == NULL || hauteur == NULL) { //sécurisation
+        return 0
+    }
 
     // Récupérer les infos de la console
-    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
-        width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-        height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE)/*récupere id console Windows*/, &csbi)) {//csbi recoit toute les infos de la fenetre
+        largeur = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        hauteur = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 
-        printf("Taille de la fenêtre de la console : %d colonnes x %d lignes\n", width, height);
+        printf("Taille de la fenêtre de la console : %d colonnes x %d lignes\n", largeur, hauteur);
     }
     else {
         printf("Erreur : Impossible de récupérer la taille de la console.\n");
--- LaureenBros/main.c
+++ LaureenBros/main.c
@@ -5,3 +5,6 @@
 #include "header.h"
 
 
+//int main() {
+//	
+//}
