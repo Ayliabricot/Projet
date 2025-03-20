dd7c27d558bf0aec8b6fb3a3938e034da7a58284
Aylia <aylia.sabadie@student.junia.com>
2025-03-20 11:31:13 +0100

    vallider

--- LaureenBros/liens.c
+++ LaureenBros/liens.c
@@ -13,6 +13,7 @@
 void lancer_ecran(int* choix, char option[6][40]) {
 	Ecran* ecran = definirEcran();
 	int touche = 0;
+	int* difficulte = 0;
 	while (1) {
 		while (*choix == 0) {
 			touche = 0;
