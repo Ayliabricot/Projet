bd4e201ee3c08848eead58fa36b562e402e994e1
Célian.Arbona <celian.arbona@student.junia.com>
2025-05-05 11:45:14 +0200

    fix map

--- LaureenBros/jeu.h
+++ LaureenBros/jeu.h
@@ -13,7 +13,7 @@
 #define SCREEN_HEIGHT 720
 #define BLOCK_SIZE 60
 #define NUMBER_OF_TILES 8
-#define MAP_WIDTH 56
+#define MAP_WIDTH 224
 #define MAP_HEIGHT 12
 #define VIEW_WIDTH 22
 #define ANIMATION_SPEED 10
